f10492b94bcb0de5044eeb49904461f82d10219a
Fahad1993 <foso1015@gmail.com>
2022-06-10 08:14:39 +0300

    authentication setup

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Form from '../components/Form'
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading'){
+    return <p>...😍</p> 
+    // return null
+  }
+
   return (
     <div className={styles.container}>
-      <Form />
-
+      {session ? (
+        <p>You are logged in!</p> 
+      ) : ( 
+      <p>You are not logged in 😞. <a href="/api/auth/signin">login</a></p>
+      )}
      
      
     </div>
